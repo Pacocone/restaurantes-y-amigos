7c6b7af92d04f79435500b9b0328fde45e4a1494
Pacocone <pacoconesa@vaquelia.com>
2025-08-22 13:04:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: "restaurantworld-86f53",
   storageBucket: "restaurantworld-86f53.firebasestorage.app",
   messagingSenderId: "459185197746",
-  appId: "1:459185197746:web:327c97c4a64f1757b56fab",
+  appId:  "1:459185197746:web:327c97c4a64f1757b56fab",
   measurementId: "G-LKKH1T8ZP7"
 }; 
   
